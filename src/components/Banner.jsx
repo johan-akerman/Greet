2bd4c104a58c96ceabe13f24cc92eaae18192235
Johan Åkerman <joha.aker@gmail.com>
2021-07-18 16:27:06 +0200

    test

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -9,7 +9,7 @@ fit for your team.</h1>
         <a href="#"
                     className="w-60 flex px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
                   >
-                    Propose a bounty
+                    Propose a prize
                   </a>
           
 
