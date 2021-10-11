10e8692afb38fc493607b569681eb6afde5eb6c8
Johan Åkerman <joha.aker@gmail.com>
2021-10-11 21:33:58 +0200

    fixed infobar

--- src/components/InfoBar.jsx
+++ src/components/InfoBar.jsx
@@ -2,14 +2,15 @@ import { Link } from "react-router-dom";
 
 export function InfoBar() {
   return (
-    <div className="md:block hidden bg-indigo-600 text-center">
+    <div className=" bg-indigo-600 text-center">
       <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
         <div className="relative">
           <p className="ml-3 text-white font-medium">
             <span>
-              👋 Greetings! We just posted our first jobs!{" "}
+              👋 Greetings! Currently <strong>10</strong> jobs and{" "}
+              <strong>210 000</strong> SEK available.{" "}
               <Link to="/job-board" className="border-b-2 ">
-                See open positions
+                See job board
               </Link>
             </span>
           </p>
